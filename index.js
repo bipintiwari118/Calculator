85e5621f76db8519bb5fd4893bfcc65c557a662a
Bunny <bipintiwari118@gmail.com>
2022-06-28 16:32:53 +0545

    Change

---
+++ index.js
@@ -0,0 +1,31 @@
+let screen= document.getElementById("screen");
+buttons= document.querySelectorAll("button");
+let screenvalue="";
+for(item of buttons){
+    item.addEventListener("click",(e)=>{
+buttonText=e.target.innerText; 
+console.log("Button text is",buttonText);
+        if(buttonText=="*"){
+            buttonText="*";
+            screenvalue+=buttonText;
+            screen.value= screenvalue;
+        }
+        else if(buttonText=="c"){
+            screenvalue="";
+            screen.value= screenvalue;
+            
+        }
+        else if(buttonText=="="){
+            screen.value=eval(screenvalue);
+        }
+        else{
+            screenvalue+=buttonText;
+            screen.value= screenvalue;
+        }
+
+
+
+
+    })
+
+}
